40a7a3a42b8f76bb91985de3e6501c00c7ba8ed1
Irsan Arisandy <irsanarisandy@hotmail.com>
2021-12-12 18:21:01 +1300

    Add navbar toggle transition for non-mobile landscape

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -8,6 +8,7 @@ import styles from '@styles/Navbar.module.scss';
 interface NavlinkProp {
   href: string;
   name: string;
+  isOpen: boolean;
   handleClick?: React.MouseEventHandler<HTMLElement>;
 }
 
@@ -16,17 +17,22 @@ interface NavbarProp {
 }
 
 interface NavbarState {
+  isMobile: boolean;
   isOpen: boolean;
+  height?: number;
+  width?: number;
 }
 
-const Navlink = ({href, name, handleClick}: NavlinkProp) => (
+const Navlink = ({href, name, isOpen, handleClick}: NavlinkProp) => (
   <div
     className="border border-gray-100 dark:border-gray-800 text-center"
     onClick={handleClick}
   >
     <Link href={href} passHref>
-      <a>
-        <span className="py-4 font-bold transition-opacity duration-500 hover:opacity-50">
+      <a style={{
+        pointerEvents: isOpen ? 'auto' : 'none'
+      }}>
+        <span className="py-4 font-bold hover:duration-500 hover:opacity-50">
           {name}
         </span>
       </a>
@@ -37,14 +43,31 @@ const Navlink = ({href, name, handleClick}: NavlinkProp) => (
 class Navbar extends React.Component<NavbarProp, NavbarState> {
   constructor(props: NavbarProp) {
     super(props);
-    this.state = { isOpen: false };
+    this.state = { isMobile: true, isOpen: false };
   }
 
-  handleClick = () => this.setState(state => ({ isOpen: !state.isOpen }));
+  componentDidMount() {
+    window.addEventListener('load', this.updateWindowDimensions);
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('load', this.updateWindowDimensions);
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions = () => this.setState({
+    height: window.innerHeight,
+    width: window.innerWidth,
+    isMobile: window.innerHeight <= 570 || window.innerWidth <= 640 || window.innerHeight >= window.innerWidth,
+    isOpen: window.innerHeight > 570 && window.innerWidth > 640 && window.innerHeight < window.innerWidth
+  });
+
+  handleClick = () => this.setState(state => ({ isOpen: state.isMobile ? !state.isOpen : true }));
 
   render() {
     const { links } = this.props;
-    const { isOpen } = this.state;
+    const { isMobile, isOpen } = this.state;
     let linkElements: JSX.Element[] = [];
     if (links != null) {
       const certainLinks = ['linkedin', 'github', 'gitlab'] as const;
@@ -57,6 +80,9 @@ class Navbar extends React.Component<NavbarProp, NavbarState> {
               rel="noopener noreferrer"
               aria-label={`${name} link`}
               key={name}
+              style={{
+                pointerEvents: isOpen ? 'auto' : 'none'
+              }}
             >
               {linkElementPair(name, styles.icon)}
             </a>
@@ -69,11 +95,16 @@ class Navbar extends React.Component<NavbarProp, NavbarState> {
         <nav
           role="navigation"
           aria-label="main navigation"
-          className={`${styles.navbar} ${isOpen ? styles.navbar_display : ''}`}
+          className={`flex flex-col ${styles.navbar} ${isOpen ? styles.navbar_display : ''}`}
         >
           <div className="flex flex-col items-center p-4">
             <Link href="/" passHref>
-              <a onClick={this.handleClick}>
+              <a
+                onClick={this.handleClick}
+                style={{
+                  pointerEvents: isOpen ? 'auto' : 'none'
+                }}
+              >
                 <Image
                   src="/images/logo.png"
                   alt="logo"
@@ -86,15 +117,15 @@ class Navbar extends React.Component<NavbarProp, NavbarState> {
             <h1 className={styles.customfont_name}>Irsan</h1>
             <span className={styles.customfont_role}>Web Developer</span>
           </div>
-          <div className='flex flex-col items-center flex-auto bg-gray-50 dark:bg-gray-900'>
+          <div className="flex flex-col items-center flex-auto bg-gray-50 dark:bg-gray-900">
             <div className="w-full my-12">
-              {/* <Navlink href="/games" name="Games" handleClick={this.handleClick} /> */}
-              <Navlink href="/coming-soon" name="About" handleClick={this.handleClick} />
-              <Navlink href="/coming-soon" name="My Skills" handleClick={this.handleClick} />
-              <Navlink href="/coming-soon" name="Blog" handleClick={this.handleClick} />
+              {/* <Navlink href="/games" name="Games" isOpen={isOpen} handleClick={this.handleClick} /> */}
+              <Navlink href="/coming-soon" name="About" isOpen={isOpen} handleClick={this.handleClick} />
+              <Navlink href="/coming-soon" name="My Skills" isOpen={isOpen} handleClick={this.handleClick} />
+              <Navlink href="/coming-soon" name="Blog" isOpen={isOpen} handleClick={this.handleClick} />
             </div>
             <div
-              className='mb-2 transition-opacity duration-1500 grid gap-3 items-center'
+              className="mb-2 grid gap-3 items-center"
               style={{
                 opacity: linkElements.length === 0 ? 0 : 1,
                 gridTemplateColumns: `repeat(${linkElements.length}, minmax(0, 1fr))`
@@ -105,11 +136,11 @@ class Navbar extends React.Component<NavbarProp, NavbarState> {
           </div>
         </nav>
         <button
-          className={`fixed top-4 right-4 text-black bg-gray-200 ${styles.toggle_button}`}
-          onClick={this.handleClick}
+          className={`text-black bg-gray-200 ${styles.toggle_nav_button}`}
+          onClick={() => this.setState({ isOpen: !isOpen })}
         >
           <span>
-            {isOpen ? '\u2613' : '\u2630'}
+            {isMobile ? (isOpen ? '\u2613' : '\u2630') : (isOpen ? '\u2B9C' : '\u2B9E')}
           </span>
         </button>
       </>
