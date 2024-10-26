bc8ac293b1125d65fb4998dd1a1b3103086e587e
Irsan Arisandy <irsanarisandy@hotmail.com>
2024-10-26 12:23:57 +1100

    Update CV link

--- data/publicLinks.ts
+++ data/publicLinks.ts
@@ -4,7 +4,7 @@ export const publicLinks: Links = {
   linkedin: 'https://www.linkedin.com/in/irsan-arisandy',
   github: 'https://github.com/irsanarisandy',
   gitlab: 'https://gitlab.com/irsanarisandy',
-  cv: 'https://drive.filen.io/d/36873c67-dca4-489b-8dd8-dc51b1c3c48a#TtV0c3lhA1ANrtIPNKevqLcd4gPxYEvW',
+  cv: 'https://drive.filen.io/d/84046d21-cb08-4b11-bc55-61e1fea7f7c7#ck0mJNAz4H6LyQ08VRg9kyXm1tEEC1xA',
   feedAtom: '/api/feeddata/atom',
   feedJson: '/api/feeddata/json',
   feedRss: '/api/feeddata/rss'
